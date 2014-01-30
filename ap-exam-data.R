265251cdb2c78f79824a53ed95dce9ae3c5283da
Brian A. Danielak <brian@briandk.com>
2014-01-30 16:49:03 -0600

    rename variables in ap data

--- ap-exam-data.R
+++ ap-exam-data.R
@@ -2,4 +2,12 @@ ap <- read.csv("AP-gender.csv",
                header = TRUE,
                stringsAsFactors = FALSE)
 
+renameVariables <- function(input) {
+  names(input)[1] <- "sex"
+  names(input) <- tolower(names(input))
+  return(input)
+}
+
+
+ap <- renameVariables(ap)
 View(ap)
